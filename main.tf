7b2be07adcaeaf6f67d897ae76e877dbfe2a49aa
yairk-create <kochavi.yair@gmail.com>
2025-08-19 08:11:40 +0000

    copied from repo

--- main.tf
+++ main.tf
@@ -1,12 +1,44 @@
+
+variable "open_webui_user" {
+  description = "Username to access the web UI"
+  default     = "admin@demo.gs"
+}
+
+variable "openai_base" {
+  description = "Optional base URL to use OpenAI API with Open Web UI" 
+  default     = "https://api.openai.com/v1"
+}
+
+variable "openai_key" {
+  description = "Optional API key to use OpenAI API with Open Web UI"
+  default     = ""
+}
+
+
+
 terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.40.0"
+      version = "3.116.0"
+    }
+    cloudinit = {
+      source = "hashicorp/cloudinit"
+      version = "2.2.0"
+    }
+     terracurl = {
+      source = "devops-rob/terracurl"
+      version = "1.2.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+    
+  }
 }
